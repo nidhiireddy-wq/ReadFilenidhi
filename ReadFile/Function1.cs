bad9eee0c93b4a78be9afadfdee9c0e97ab09e85
Inno <Inno@DESKTOP-CRJJOV8>
2026-09-17 18:10:47 +0530

    Brnanch1

--- ReadFile/Function1.cs
+++ ReadFile/Function1.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.IO;
 
-namespace ReadFile;
+namespace TestFunc;
 
 public class Function1
 {
     private readonly ILogger<Function1> _logger;
+    private readonly IConfiguration _configuration;
 
-    public Function1(ILogger<Function1> logger)
+    public Function1(ILogger<Function1> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     [Function("Function1")]
@@ -21,11 +24,11 @@ public class Function1
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        string filePath = @"C:\Users\Inno\source\repos\sample.txt";
+        string filePath = _configuration["SamplePath"];
 
         if (!File.Exists(filePath))
         {
-            return new NotFoundObjectResult("File not found!");
+            return new NotFoundObjectResult("File not found.");
         }
 
         string fileContent = File.ReadAllText(filePath);
