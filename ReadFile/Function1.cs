722feca5348ed3fdc70984ede3425ae62da56cf5
Inno <Inno@DESKTOP-CRJJOV8>
2026-09-17 17:57:44 +0530

    Add project files.

---
+++ ReadFile/Function1.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace ReadFile;
+
+public class Function1
+{
+    private readonly ILogger<Function1> _logger;
+
+    public Function1(ILogger<Function1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("Function1")]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        string filePath = @"C:\Users\Inno\source\repos\sample.txt";
+
+        if (!File.Exists(filePath))
+        {
+            return new NotFoundObjectResult("File not found.");
+        }
+
+        string fileContent = File.ReadAllText(filePath);
+
+        return new OkObjectResult(fileContent);
+    }
+}
